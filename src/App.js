17bd10c3bd2e7eb575bf46f625bb91541d3a62b6
Hzxon <110453498+Hzxon@users.noreply.github.com>
2025-05-10 21:29:03 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Hello World!</code> Bulutangkis Cabang UMN
+          <code>Hello World!</code> Bulutangkis Cabang UMN
         </p>
         <a
           className="App-link"
