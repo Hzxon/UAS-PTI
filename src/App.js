a12326f152782c7e69ff02b323e5d2a7ce57e701
Wilbert Lincoln <lincolnwilbert27@gmail.com>
2025-05-10 22:55:12 +0700

    branch gua

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           <code>Hello World!</code> Bulutangkis Cabang UMN
+          test branch
         </p>
         <a
           className="App-link"
